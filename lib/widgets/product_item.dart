a9d80828167b4682fa8b1ccb603d5b7b08bb0ef7
Snehil <snehilsneh888@gmail.com>
2021-09-20 17:42:12 +0530

    Some nav stuff

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../screens/product_detail_screen.dart';
+
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
@@ -11,21 +13,30 @@ class ProductItem extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,arguments:id, );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(icon: Icon(Icons.favorite), onPressed: () {},
-          color: Theme.of(context).accentColor,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
           ),
           title: Text(
             title,
             textAlign: TextAlign.center,
           ),
-          trailing: IconButton(icon: Icon(Icons.shopping_bag), onPressed: () {},
-          color: Theme.of(context).accentColor,
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_bag),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
           ),
         ),
       ),
